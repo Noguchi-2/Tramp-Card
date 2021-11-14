f72c523a5e79ad0e3d1455e9492f8e0a832afd58
KohagiNoguchi <Kohagi2006585@gmail.com>
2021-11-14 14:54:59 +0900

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Tramp Card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tramp Card
+//
+//  Created by Kohagi on 2021/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
